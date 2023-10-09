4e238898faa36c33af824d7e2544ab1b860e60e4
mohamedaz025 <mohamedaz025@gmail.com>
2023-10-09 15:06:26 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: test4app (),
+    );
+  }
+}
+
+
+
+class test4app extends StatelessWidget {
+  const test4app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar( 
+  title: Text("belquis",style: TextStyle(color: Colors.orange,fontSize: 30,fontWeight: FontWeight.w700),),
+  leading: IconButton(icon: Icon(Icons.menu, size: 30 ),
+        onPressed:(){}), 
+  actions: [
+    IconButton(onPressed: ()  {}, icon: Icon(Icons.mail)) 
+  ],  
+
+  backgroundColor: Colors.grey[20],
+  shadowColor: Colors.black,
+  elevation: 20,
+  centerTitle: true,
+),
+
+  body: 
+      Container(
+        color: Colors.grey[500],
+         width: 395,
+         height: 300,
+         child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                alignment: Alignment.center,
+                child: Text("belquis", style: TextStyle(fontSize: 20,),),
+                
+                color: Colors.blue ,
+                width: 100,
+                height: 100,
+                ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text("belquis", style: TextStyle(fontSize: 20,),),
+                  
+                color: Colors.red ,
+                width: 100,
+                height: 100,
+                ),
+            ),
+            Expanded(
+              flex:3,
+            child: Container(
+                alignment: Alignment.center,
+                child: Text("belquis", style: TextStyle(fontSize: 20,),),
+              
+              color: Colors.yellow,
+              width: 100,
+              height:100,
+              ),)
+
+             
+          ],
+         ),
+      )
+    
+
+    );
+  }
+}
